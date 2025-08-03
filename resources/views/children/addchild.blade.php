c40ff930f99f9b8c34c7cfc080bfe43ccee6a6f1
gmirilla <gmirilla@gmail.com>
2025-08-03 10:44:57 +0100

    dispaly changes to child view

--- resources/views/children/addchild.blade.php
+++ resources/views/children/addchild.blade.php
@@ -1,15 +1,3 @@
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
-
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-
-<link rel="stylesheet" href=
-"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 <x-layouts.app>
 @if ($errors->any())
   <div class="alert alert-danger">
@@ -119,4 +107,29 @@
     
     </div>
 </div>
+<script>
+  $(document).ready(function () {
+    $('.table').DataTable({
+      dom: 'Bfrtip',
+      buttons: ['copy', 'csv', 'excel', 'pdf']
+    });
+  });
+</script>
+<script>
+  document.querySelector('input[name="profilepicture"]').addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(evt) {
+        const preview = document.createElement('img');
+        preview.src = evt.target.result;
+        preview.style.maxWidth = '150px';
+        e.target.parentNode.appendChild(preview);
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+</script>
+
+
 </x-layouts.app>
