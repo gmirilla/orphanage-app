9ec6ca9cf8ec730a683cc0630b67c07626bc1019
gmirilla <gmirilla@gmail.com>
2025-08-03 10:37:12 +0100

    display changes to the add child view

--- resources/views/children/addchild.blade.php
+++ resources/views/children/addchild.blade.php
@@ -32,39 +32,35 @@
             @csrf
 
         <div class="row mb-3">
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="fname" class="form-label">First Name</label>
                 <input type="text" name='fname' id='fname' required placeholder="Enter Child's First Name" class="form-control">
             </div>
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="mname" class="form-label">Middle Name</label>
                 <input type="text" name='mname' id='mname' placeholder="Enter Child's Middle Name or initials" class="form-control">
             </div>
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="lname" class="form-label">Last Name</label>
                 <input type="text" required   name='lname' id='lname' placeholder="Enter Child's Last Name" class="form-control">
             </div>
-
-        </div>
-        <div class="row mb-3">
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="dateofbirth" class="form-label">Date of Birth</label>
                 <input type="date" name='dateofbirth' id='dateofbirth' placeholder="Date of Birth" class="form-control">
             </div>
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="gender"  class="form-label">Gender</label>
                 <select name="gender" required   id="gender" class="form-select">
                     <option value="Female">Female</option>
                     <option value="Male">Male</option>
                 </select>
             </div>
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="birthplace"  class="form-label">Place of Birth</label>
                 <input type="text" required name='birthplace' id='birthplace' placeholder="Place of Birth" class="form-control">
             </div>
         </div>
-        <div class="row mb-3">
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="nationalityid" class="form-label">Nationality</label>
                 <select name="nationalityid"  required id="nationalityid" class="form-select">
                     @forelse ($countries as $country )
@@ -74,7 +70,7 @@
                     @endforelse
                 </select>
             </div>
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="identificationtype"   class="form-label">Identification Type</label>
                 <select name="identificationtype" required id="identificationtype" class="form-select">
                     <option value="Birth Certificate">Birth Certificate</option>
@@ -82,18 +78,16 @@
                     <option value="None">None</option>
                 </select>
             </div>
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="identificationno" class="form-label">Identification Number</label>
                 <input type="text" name='identificationno' id='identificationno' placeholder="Identification number" class="form-control">
             </div>
-        </div>
-        <div class="row">
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="admissiondate" class="form-label">Admission Date</label>
                 <input type="date" name='admissiondate' id='admissiondate' placeholder="Admission Date" class="form-control">
             </div>
 
-            <div class="col-2">
+            <div class="col-auto">
                 <label for="referalsource" class="form-label">Referal Source</label>
                 <select name="referalsource" id="referalsource" class="form-select">
                     <option value="Hospital">Hospital</option>
@@ -106,13 +100,11 @@
                 </select>
             </div>
 
-            <div class="col-2">
+            <div class="col-auto">
                    <label for="profilepicture" class="form-label">Upload Picture</label>
                     <input type="file" name="profilepicture" accept="image/*" class="form-control">
             </div>
-        </div>
-        <div class="row">
-            <div class="col-6">
+            <div class="col-auto">
                 <label for="note" class="form-label">Remark(s)</label>
                  <textarea name="note" id="note" cols="30" rows="5" placeholder="Enter any notes or remarks" class="form-control"></textarea>
             </div>
