0d7f2ddd235779e56dfdaabd481f71118d2aa37f
gmirilla <gmirilla@gmail.com>
2025-12-09 19:29:49 +0100

    changes to login page

--- resources/views/components/layouts/auth/split.blade.php
+++ resources/views/components/layouts/auth/split.blade.php
@@ -2,16 +2,25 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
+        <style>
+                    .daralamah-yellow {
+            background-color: #B7B362; /* Tailwind's text-neutral-300 */
+        }
+
+        .daralamah-green {
+            background-color: #324b45; /* Dark green color */
+        }
+    </style>
     </head>
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+    <body class="min-h-screen daralamah-green">
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-                <div class="absolute inset-0 bg-neutral-900"></div>
+                <div class="absolute inset-0 daralamah-yellow"></div>
                 <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
                     <span class="flex h-10 w-10 items-center justify-center rounded-md">
                         <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
                     </span>
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Daralamana') }}
                 </a>
 
                 @php
@@ -32,7 +41,7 @@
                             <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
                         </span>
 
-                        <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                        <span class="sr-only">{{ config('app.name', 'Daralamana') }}</span>
                     </a>
                     {{ $slot }}
                 </div>
